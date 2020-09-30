5967ec12f5a246258baeeb51db4414e48320e146
Matheus Bello <mathbello@gmail.com>
2020-09-30 14:38:24 -0300

    Add exercicios Spring

---
+++ minhaSegundaAplicacao/src/main/java/com/generation/minhaSegundaAplicacao/MinhaSegundaAplicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.minhaSegundaAplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaSegundaAplicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaSegundaAplicacaoApplication.class, args);
+	}
+
+}
---
+++ minhaSegundaAplicacao/src/test/java/com/generation/minhaSegundaAplicacao/MinhaSegundaAplicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.minhaSegundaAplicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaSegundaAplicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
