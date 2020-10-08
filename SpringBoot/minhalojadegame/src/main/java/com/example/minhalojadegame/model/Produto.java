7140a97a1f65aabf31ff9214362a3ac7d79759e7
Matheus Bello <mathbello@gmail.com>
2020-10-08 14:38:59 -0300

    Add crud minhaLojaDeGames

---
+++ SpringBoot/minhalojadegame/src/main/java/com/example/minhalojadegame/model/Produto.java
@@ -0,0 +1,91 @@
+package com.example.minhalojadegame.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min= 5, max= 100)
+	private String nome;
+	
+	@NotNull
+	@Size(min= 5, max= 100)
+	private String modelo;
+	
+	@NotNull
+	private int quantidade;
+	
+	@NotNull
+	@DecimalMin("5,2")
+	@DecimalMax("7,2")
+	private double valor;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("categoria")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
---
+++ SpringBoot/minhalojadegame/src/test/java/com/example/minhalojadegame/MinhalojadegameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minhalojadegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhalojadegameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
